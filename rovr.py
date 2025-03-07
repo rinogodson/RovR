66cd88fe58acf13c6809da6c15f57bd5e4677119
Rino Godson <rinogodson@gmail.com>
2025-03-07 09:40:42 +0530

    Search Functionality Complete

--- rovr.py
+++ rovr.py
@@ -6,9 +6,11 @@ dotFiles = False
 def main(stdscr):
 
   curses.start_color()
+  curses.use_default_colors()
   curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
   curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
   curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLUE)
+  curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
   curses.curs_set(0)
   
@@ -20,13 +22,27 @@ def main(stdscr):
   height, width = stdscr.getmaxyx()
   max_visible_items = height - 6
   scroll_offset = max(0, f_index - max_visible_items + 1)
+  search_query = ""
 
   while True:
     stdscr.clear()
+
     if f_index < scroll_offset:
       scroll_offset = f_index
     elif f_index >= scroll_offset + max_visible_items:
       scroll_offset = f_index - max_visible_items + 1
+
+
+    if search_query:
+      f_list = searchResults(search_query, f_list)
+    else:
+      f_list = filterList(sorted(os.listdir(c_dir)), c_dir);
+
+    if not f_list:
+      f_list = ["(No Results Found)"]
+      f_index = 0
+
+    stdscr.addstr(height - 3, 2, f"🔍: {search_query}".ljust(width - 6), curses.color_pair(4))
 
     stdscr.border(0)
     stdscr.addstr(0, 2, " RovR 🛰 ", curses.A_BOLD)
@@ -59,13 +75,31 @@ def main(stdscr):
       f_index -= 1 
     elif key == curses.KEY_DOWN and f_index < len(f_list) - 1:
       f_index += 1  
-    elif key in (ord('q'), 27):
-      break    
+    elif key == 27:
+      if search_query:
+        search_query = ""
+      else:
+        break
+    elif key in (curses.KEY_BACKSPACE, 127):
+      search_query = search_query[:-1]
+      f_index = 0
+
+    #TODO: ADD DOT FILES TOGGLE
+    # elif key == 46:  # ASCII code for "."
+    #   next_key = stdscr.getch()  # Get the next key press
+    #   if next_key == 27:
+    #       global dotFiles
+    #       dotFiles = not dotFiles
+    #       f_list = filterList(sorted(os.listdir(c_dir)), c_dir)
+    #       f_index = 0
+
+    elif 32 <= key <= 126:
+      search_query += chr(key)
+      f_index = 0
 
     elif key == curses.KEY_RIGHT:
       selected_item = f_list[f_index]
       new_path = os.path.join(c_dir, selected_item)
-
       if(os.path.isdir(new_path)):
         try:
           history.append(c_dir)
@@ -76,6 +110,7 @@ def main(stdscr):
           f_index = 0
         except PermissionError:
             stdscr.addstr(len(f_list) + 4, 2, "🚫 Access Denied", curses.color_pair(1))
+      search_query = ""
 
     elif key == curses.KEY_LEFT:
       parent_dir = os.path.dirname(c_dir)
@@ -86,6 +121,7 @@ def main(stdscr):
       f_list = filterList(sorted(os.listdir(c_dir)), c_dir)
       f_index = f_list.index(dir_name) if dir_name in f_list else 0
       history.append(dir_name)
+      search_query = ""
 
     stdscr.refresh()
 
@@ -98,5 +134,12 @@ def filterList(arr, c):
 
 def sort_folders_first(file_list, current_path):
     return sorted(file_list, key=lambda item: (not os.path.isdir(os.path.join(current_path, item)), item.lower()))
+
+def searchResults(searchQ, file_list):
+    if not searchQ:
+        return file_list
+
+    return [item for item in file_list if searchQ.lower() in item.lower()]
+
 
 curses.wrapper(main)
